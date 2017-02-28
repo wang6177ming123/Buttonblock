ebdf39594e2e71d2f932b8798e99b74c07de9859
pactera <pactera@pactera2222222.local>
2017-02-28 16:12:39 +0800

    Initial Commit

---
+++ Buttonblock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Buttonblock
+//
+//  Created by pactera on 17/2/28.
+//  Copyright © 2017年 pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
